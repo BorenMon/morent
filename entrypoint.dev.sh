d961997a3144d27c6398bbf1119eeb6d0e03db04
Mon Boren <63392246+BorenMon@users.noreply.github.com>
2025-01-26 00:13:03 +0700

    Update

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -2,4 +2,4 @@
 
 composer install --no-dev --optimize-autoloader
 
-npm install && npm run dev & php artisan serve --host=0.0.0.0 --port=7000
+npm install && npm run dev & php artisan serve --host=0.0.0.0
