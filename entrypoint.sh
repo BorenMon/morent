e95b7ee8f179a91167106796ce44443897eb67ec
Mon Boren <63392246+BorenMon@users.noreply.github.com>
2025-01-26 02:35:09 +0700

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 composer install --optimize-autoloader
 
+chmod 777 /storage/logs
+chmod 777 /storage/frameworks/sessions
+chmod 777 /storage/frameworks/views
+
 yarn install && yarn build
 
 php-fpm
