ab7debcbdb95651ebaf7a4b91897d6a13a78e657
BOREN <borenmon5@gmail.com>
2025-02-06 23:03:50 +0700

    Update

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,6 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request)
     {
-
         $request->authenticate();
 
         $request->session()->regenerate();
