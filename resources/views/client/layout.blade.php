2660c634802b85852297c937a476b59c802eaece
Mon Boren <borenmon5@gmail.com>
2025-02-13 08:39:17 +0700

    Update

--- resources/views/client/layout.blade.php
+++ resources/views/client/layout.blade.php
@@ -55,8 +55,12 @@
                         <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
                             role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                             <div class="py-1" role="none">
+                                @if ($user->role == UserRole::Admin->value)
+                                    <a href="{{ route('admin.dashboard') }}" class="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                                    role="menuitem" tabindex="-1" id="menu-item-0">Admin Dashboard</a>
+                                @endif
                                 <a href="/profile" class="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                                    role="menuitem" tabindex="-1" id="menu-item-0">Profile Setting</a>
+                                    role="menuitem" tabindex="-1" id="menu-item-1">Profile Setting</a>
                                 <div class="block px-4 py-2 text-gray-700 hover:bg-gray-100 cursor-pointer logout"
                                     role="menuitem" tabindex="-1">
                                     Logout
