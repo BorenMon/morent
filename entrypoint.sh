f909e10778302d6ee84fa9a59eac22c6ac9d3c0b
Mon Boren <borenmon5@gmail.com>
2025-03-13 12:00:09 +0700

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,15 @@
 
 composer update && composer install --optimize-autoloader
 
-chmod 777 storage/logs
-chmod 777 storage/framework/sessions/*
-chmod 777 storage/framework/views
+# chmod 777 storage/logs/laravel.log
+# chmod 777 storage/framework/sessions
+# chmod 777 storage/framework/views
+# chmod 777 storage/framework/cache/data/*
+chown -R www-data:www-data /var/www/html/storage
+chmod -R 775 /var/www/html/storage
 
 rm -rf public/hot
 
 yarn && yarn build
 
 apache2-foreground
-# & php artisan queue:work --daemon
